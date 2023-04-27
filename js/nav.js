fb655d7089075177ed5378230d284f86200823bc
Joel Andersen <joel.andersen24@gmail.com>
2023-04-27 05:52:24 -0400

    Project Submission

---
+++ js/nav.js
@@ -0,0 +1,79 @@
+"use strict";
+
+/******************************************************************************
+ * Handling navbar clicks and updating navbar
+ */
+
+/** Show main list of all stories when click site name */
+
+function navAllStories(evt) {
+  console.debug("navAllStories", evt);
+  hidePageComponents();
+  putStoriesOnPage();
+}
+
+$body.on("click", "#nav-all", navAllStories);
+
+// Show Submit form on click "submit"
+
+function navSubmitStoryClick(e) {
+  console.debug("navSubmitStoryClick", e);
+  hidePageComponents();
+  $allStoiresList.show();
+  $submitForm.show();
+}
+
+$navSubmitStory.on("click", navSubmitStoryClick);
+
+//  SHow favorites on click "favorite"
+
+function navFavoritesClick(e) {
+  console.debug("navFavoritesClick", e);
+  hidePageComponents();
+  putFavoritesListOnPage();
+}
+
+$body.on("click", "#nav-favorites", navFavoritesClick);
+
+// Show OP stories on click "my stories"
+
+function navMyStories(e){
+  console.debug("navMyStories", e)
+  hidePageComponents();
+  putOwnStoriesOnPage();
+  $ownStories.show();
+}
+
+$body.on("click", "nav-my-stories", navMyStories);
+
+/** Show login/signup on click on "login" */
+
+function navLoginClick(evt) {
+  console.debug("navLoginClick", evt);
+  hidePageComponents();
+  $loginForm.show();
+  $signupForm.show();
+  $storiesContainer.hide();
+}
+
+$navLogin.on("click", navLoginClick);
+
+// Show only profile on click "profile"
+
+function navProfileClick(e) {
+  console.debug("navProfileClick", e);
+  hidePageComponents();
+  $userProfile.show();
+}
+
+$navUserProfile.on("click", navProfileClick);
+
+/** When a user first logins in, update the navbar to reflect that. */
+
+function updateNavOnLogin() {
+  console.debug("updateNavOnLogin");
+  $(".main-nav-links").show();
+  $navLogin.hide();
+  $navLogOut.show();
+  $navUserProfile.text(`${currentUser.username}`).show();
+}
